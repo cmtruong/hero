2cdac9a8370c8444975b75071923d90ae5f2aab9
cmtruong <cong-minh.truong@capgemini.com>
2016-11-24 11:33:49 +0100

    Up to level 5

--- hero.js
+++ hero.js
@@ -2,8 +2,9 @@ var name 		= prompt("Please define your hero's name:");
 document.write(name + "<br/>");
 
 // Skill: prompt()
-var level 		= 1;
-var xp 			= 0;
+var level 		= 4;
+var xp 			= 300;
+var health 		= level * 100;
 var skill_point = 5;
 var male 		= true;
 var caracter_hero 	= "Dark Knight";
@@ -19,8 +20,7 @@ var intel 	= 0.2 * skill_point + 10 * level;
 // // Skill: controle.log()
 
 var caracter = prompt("Please choose your caracter: a|DK, b|Wizard, c|Archer");
-console.log(caracter);
-level = level + 3;
+
 if (caracter === 'a') {
 	caracter_hero = 'Dark Knight';
 	strengh = strengh + 10;
@@ -38,8 +38,70 @@ if (caracter === 'a') {
 	console.log("Error in the game. Pls contact your game master");
 }
 
+// Skill Math.random()
+// Skill alert()
+
+var dragonHealth = 600;
+var dragonStr = 100;
+var dragonAgi = 15;
+var dragonInt = 80;
+
 console.log(level);
 console.log(caracter_hero);
+document.write("Your health: " + health + "<br/>");
 document.write("Your strengh: " + strengh + "<br/>");
 document.write("Your agility: " + agility + "<br/>");
 document.write("Your intel: " + intel + "<br/>");
+
+var end_game = false;
+
+
+while(!end_game){
+	// Hero's turn
+	var hero_hit_point = Math.random()*100;
+	if (hero_hit_point > dragonAgi) {
+		dragonHealth = dragonHealth - strengh;
+		alert("Your hit is " + strengh + " damage");
+		console.log("Dragon health: " + dragonHealth);
+		var hero_critical = Math.random() * 100;
+		if (hero_critical < intel) {
+			dragonHealth = dragonHealth - strengh * 2;
+			alert("Crit! Dragon got " + strengh*2 + " damage");
+			console.log("Dragon health: " + dragonHealth);
+		}
+	} else {
+		alert("Misssssss");
+		console.log("Dragon health: " + dragonHealth);
+	}
+
+	// Dragon's turn
+	var dragon_hit_point = Math.random()*100;
+	if (dragonHealth <= 0) {
+		end_game = true;
+		alert("Mission completed");
+		console.log(health);
+	} else {
+		if (dragon_hit_point > agility) {
+			health = health - dragonStr;
+			alert("Dragon fire! You've got " + dragonStr + " damage");
+			console.log("Your health: " + health);
+			var dragon_critical = Math.random()*100;
+			if (dragon_critical < dragonInt) {
+				health = health - dragonStr*2;
+				alert("Damm it! You've got a critical with " + dragonStr*2 + " dammage");
+				console.log("Your health: " + health);
+			}
+		} else {
+			alert("Misss! Damm it, you are so lucky");
+			console.log("Your health: " + health);
+		}
+	}
+
+	if (health <= 0) {
+		end_game = true;
+		alert("Mission failed. Pls try it later");
+		console.log(dragonHealth);
+	}
+}
+
+
